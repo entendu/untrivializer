828fbb02ac886133d1740d2d87b3030f17696b8a
Domenic Santangelo <domenics@gmail.com>
2017-11-21 12:42:03 -0800

    v1.0.0

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
-module.exports = (robot) => {
-  // Your code here
-  console.log('Yay, the app was loaded!')
+module.exports = robot => {
+  robot.on('issue_comment', async context => {
+    // Matches comments like, "This should be simple", "The is totally trivial",
+    // "This is super trivial", etc.
+    const regex = /(should be|would be|could be|is) (|totally|completely|very|super|probably)(| )(simple|easy|trivial)/gi;
+    const str = context.payload.comment.body;
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+    let m;
+    let aMatches = [];
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
-}
+    while ((m = regex.exec(str)) !== null) {
+      // This is necessary to avoid infinite loops with zero-width matches
+      if (m.index === regex.lastIndex) {
+        regex.lastIndex++;
+      }
+      if (m.length > 0) {
+        aMatches.push(m[0]);
+      }
+    }
+
+    if (aMatches.length > 0 && context.payload.comment.user.type != "Bot") {
+      try {
+        const tbody = "> " + aMatches.join("\n\n> ")
+                  + "\n\nDid you mean, \"_might be straightforward, but could have unforseen complexities that would completely change the prioritization of the issue, so we should let it go through the normal planning and estimation process_\"[?](https://en.wikipedia.org/wiki/Planning_fallacy)";
+        context.github.issues.createComment(context.issue({body: tbody}))
+      }
+      catch (err) {
+        if (err.code !== 404) {
+          throw err
+        }
+      }
+    }
+
+  });
+};
